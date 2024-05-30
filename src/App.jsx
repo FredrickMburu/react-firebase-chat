ad7071c660312f5cbd099e64052a9ce14470a9a5
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2024-05-31 01:21:01 +0300

    done with the chatlist and userInfo frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Chat from "./chat/Chat"
+import Detail from "./detail/Detail"
+import List from "./list/List"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
